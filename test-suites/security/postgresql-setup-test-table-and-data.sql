42864dc69dd5c9d8099b5bfdffd0611f18b6c58e
sriyazahmed <37499543+sriyazahmed@users.noreply.github.com>
2018-06-22 16:45:09 +0530

    Update postgresql-setup-test-table-and-data.sql

--- test-suites/security/postgresql-setup-test-table-and-data.sql
+++ test-suites/security/postgresql-setup-test-table-and-data.sql
@@ -1,4 +1,17 @@
+/* create the database testdb for inserting test data for sql injection testing */
+CREATE DATABASE testdb
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE testdb
+    IS 'test db for testing sql injection ';
+
+/* drop table if exists */
 DROP TABLE COMPANY;
+
+/* create table company */
 
 CREATE TABLE COMPANY(
    ID INT PRIMARY KEY     NOT NULL,
@@ -9,9 +22,12 @@ CREATE TABLE COMPANY(
    JOIN_DATE	  DATE
 );
 
+/* insert data into table company */
+
 INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY,JOIN_DATE) VALUES (1, 'Paul', 32, 'California', 20000.00,'2001-07-13');
 INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,JOIN_DATE) VALUES (2, 'Allen', 25, 'Texas', '2007-12-13');
 INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY,JOIN_DATE) VALUES (3, 'Teddy', 23, 'Norway', 20000.00, DEFAULT );
 INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY,JOIN_DATE) VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00, '2007-12-13' ), (5, 'David', 27, 'Texas', 85000.00, '2007-12-13');
 
+/* check the data in the table */
 SELECT * FROM COMPANY;
